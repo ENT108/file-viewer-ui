b552e9b03fbc356e34f352a27c8cd5063657f1fe
Adam Klukowski <adam.klukowski@pronos.eu>
2020-06-05 13:40:32 +0200

    incrementa update

--- src/App.js
+++ src/App.js
@@ -4,38 +4,58 @@ import "./App.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import FormControl from "react-bootstrap/FormControl";
 import ListGroup from "react-bootstrap/ListGroup";
 import dataSource from "./dataSource.json";
 import { parseFiles } from "./parseFiles";
 
 function App() {
   const [preview, setPreview] = useState(null);
+  const [filter, setFilter] = useState(null);
   const data = parseFiles(dataSource);
-  const files = Array.from(parseFiles(dataSource).keys());
+  const files = Array.from(parseFiles(dataSource).keys())
+    .map((file) => {
+      if (filter && file.toLowerCase().search(filter) > -1) return file;
+      else if (!filter) return file;
+      else return null;
+    })
+    .filter((file) => file);
 
   return (
-    <div className="App">
-      <Container fluid className="mt-5">
-        <Row>
-          <Col xs={12} md={2}>
-            <ListGroup>
-              {files.map((file, idx) => (
-                <ListGroup.Item
-                  onClick={() => setPreview(file)}
-                  action
-                  key={idx}
-                >
-                  {file}
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
+    <Container fluid className="mt-5">
+      <Row>
+        <Col xs={12} md={3}>
+          <FormControl
+            placeholder="Search..."
+            aria-label="Search"
+            className="mb-2"
+            onChange={(e) => setFilter(e.target.value)}
+          />
+          <ListGroup>
+            {files.map((file, idx) => (
+              <ListGroup.Item
+                variant="dark"
+                onClick={() => setPreview(file)}
+                action
+                key={idx}
+              >
+                {file}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Col>
+        {preview && (
+          <Col>
+            <div className="rounded">
+              {/* <div onClick={() => setFilter(null)}>
+              <span role="img">❌</span>
+            </div> */}
+              <code className="output">{data.get(preview)}</code>
+            </div>
           </Col>
-          <Col xs={12} md={10} className="border rounded">
-            <code>{data.get(preview)}</code>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+        )}
+      </Row>
+    </Container>
   );
 }
 
