808b8a22621145633fd6573b282b1bb4b79fc6b5
Adam Klukowski <adam.klukowski@pronos.eu>
2020-06-05 16:34:25 +0200

    added scripts

--- src/components/FileList.jsx
+++ src/components/FileList.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import ListGroup from "react-bootstrap/ListGroup";
-
 import { SentencesEnum } from "../constants";
 
 const FileList = ({ files, filter, setFilter, setPreview }) => (
