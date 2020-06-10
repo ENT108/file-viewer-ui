f05fabdd865533f9d00c268c93faa10744ae03ea
Adam Klukowski <adam.klukowski@pronos.eu>
2020-06-10 10:37:19 +0200

    formatting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import dataSource from "./data/dataSource.json";
-import { parseFiles } from "./tools/dataTools";
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import dataSource from "./data/dataSource.json"
+import {parseFiles} from "./tools/dataTools"
 
-const data = parseFiles(dataSource);
+const data = parseFiles(dataSource)
 
 ReactDOM.render(
   <React.StrictMode>
     <App data={data} />
   </React.StrictMode>,
   document.getElementById("root")
-);
+)
